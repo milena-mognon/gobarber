3e56eae07ed14babc336b3dcd20049b146e0a35c
milena-mognon <milenamognon@gmail.com>
2020-06-27 16:28:21 -0300

    list providers and tests

---
+++ backend/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('Create User', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const provider1 = await fakeUsersRepository.create({
+      name: 'Provider 1',
+      email: 'provider1@mail.com',
+      password: '123456',
+    });
+    const provider2 = await fakeUsersRepository.create({
+      name: 'Provider 2',
+      email: 'provider2@mail.com',
+      password: '123456',
+    });
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Provider 3',
+      email: 'provider3@mail.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([provider1, provider2]);
+    // different ways of the same test.
+    expect(providers).not.toContain(loggedUser);
+  });
+});
