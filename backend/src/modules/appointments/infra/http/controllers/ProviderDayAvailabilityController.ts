f497e02ec1ffae874bae32472bf400bda96e6b96
milena-mognon <milenamognon@gmail.com>
2020-07-02 17:25:17 -0300

    routes and controllers of providers availability

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, year, day } = request.body;
+
+    const listProvidersDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProvidersDayAvailability.execute({
+      provider_id,
+      month,
+      year,
+      day,
+    });
+
+    return response.json(availability);
+  }
+}
+
+export default new ProviderDayAvailabilityController();
--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthentidated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 
 const providersRouter = Router();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', ProvidersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  ProviderMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  ProviderDayAvailabilityController.index,
+);
 
 export default providersRouter;
