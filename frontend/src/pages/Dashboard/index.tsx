00b260f5f77a5bcda21b2ad08d5864e9ee79d269
milena-mognon <milenamognon@gmail.com>
2020-07-17 11:10:47 -0300

    styles - appoitments list

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointments,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
+  const [selectdDate, setSelectedDate] = useState(new Date());
 
   return (
     <Container>
@@ -28,6 +40,87 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointments>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/38504845?s=460&u=e62cabf33d90c5d6ded18d4ffc772a8c4bc5f5bb&v=4"
+                alt="Milena Mognon"
+              />
+
+              <strong>Milena Mognon</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointments>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/38504845?s=460&u=e62cabf33d90c5d6ded18d4ffc772a8c4bc5f5bb&v=4"
+                  alt="Milena Mognon"
+                />
+
+                <strong>Milena Mognon</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/38504845?s=460&u=e62cabf33d90c5d6ded18d4ffc772a8c4bc5f5bb&v=4"
+                  alt="Milena Mognon"
+                />
+
+                <strong>Milena Mognon</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/38504845?s=460&u=e62cabf33d90c5d6ded18d4ffc772a8c4bc5f5bb&v=4"
+                  alt="Milena Mognon"
+                />
+
+                <strong>Milena Mognon</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
